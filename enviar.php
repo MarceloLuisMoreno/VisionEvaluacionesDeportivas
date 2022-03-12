ac978cb9891343f90252ba7140fcec0c21a9afe1
Backend-CoderHouse <marceloluismoreno.gmail.com>
2022-03-12 13:05:04 -0300

    Correcciones de colores

--- enviar.php
+++ enviar.php
@@ -15,7 +15,7 @@ $mensaje .= "Su e-mail es: " . $mail . "\r\n";
 $mensaje .= "Mensaje: " . $comentario . "\r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time()). "\r\n";
 
-$para = 'elevenflow995@gmail.com';
+$para = 'visionevaluaciones@gmail.com';
 $asunto = 'Mensaje de mi sitio web VISION EVALUACIONES DEPORTIVAS';
 
 if (mail($para, $asunto, utf8_decode($mensaje), $header))
