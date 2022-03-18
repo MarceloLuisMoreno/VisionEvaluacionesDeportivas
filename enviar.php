66223404d30f2f28ebe1e9108747caa323b655ff
Backend-CoderHouse <marceloluismoreno.gmail.com>
2022-03-18 15:51:35 -0300

    Visión Evaluaciones, correcion whatsapp, email y Linkedin.

--- enviar.php
+++ enviar.php
@@ -15,7 +15,7 @@ $mensaje .= "Su e-mail es: " . $mail . "\r\n";
 $mensaje .= "Mensaje: " . $comentario . "\r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time()). "\r\n";
 
-$para = 'visionevaluaciones@gmail.com';
+$para = 'mailto:vision.evaluacionesdeportivas@gmail.com';
 $asunto = 'Mensaje de mi sitio web VISION EVALUACIONES DEPORTIVAS';
 
 if (mail($para, $asunto, utf8_decode($mensaje), $header))
